f0f57409a5a217353580d174365032e1f02f2a47
ajisolicha <43943999+ajisolicha@users.noreply.github.com>
2018-11-02 16:42:08 +0700

    'pusatMainWindow'

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+#include <iostream>
+#include <stdio.h>
+
+
+
+int main(int argc, char *argv[])
+{
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+
+    return a.exec();
+}
+
+
+
+
