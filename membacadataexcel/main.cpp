bfd185ad193f5ccbc9f214a08d25753531cc44b8
ariku <ariksugianto01@gmail.com>
2018-11-07 14:08:20 +0700

    Membaca data excel dan menampilkan dalam bentuk string

--- membacadataexcel/main.cpp
+++ membacadataexcel/main.cpp
@@ -3,24 +3,36 @@
 #include <QAxObject>
 #include <QSqlQuery>
 #include <QDebug>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    auto excel     = new QAxObject("Excel.Application");
-    auto workbooks = excel->querySubObject("Workbooks");
+    Mat image =imread("D:\\Capture gambar\\gambar.png");
+    QAxObject* excel     = new QAxObject("Excel.Application");
+    QAxObject* workbooks = excel->querySubObject("Workbooks");
     QString d_filename = "C:\\Users\\Ariku\\Documents\\Data.xlsx";
-    auto workbook  = workbooks->querySubObject("Open(const QString&)",d_filename);
-    auto sheets    = workbook->querySubObject("Worksheets");
-    auto sheet     = sheets->querySubObject("Item(int)", 1);
+    QAxObject* workbook  = workbooks->querySubObject("Open(const QString&)",d_filename);
+    QAxObject* sheets    = workbook->querySubObject("Worksheets");
+    QAxObject* sheet     = sheets->querySubObject("Item(int)", 1);
 
     // read the first cells in row 1...18177
-    for (int r = 1; (r <= 18177); ++r)
+    for (int r = 1; (r <= 1); ++r)
     {
-        auto cCell = sheet->querySubObject("Cells(int,int)",r,1);
-        qDebug() << cCell->dynamicCall("Value()").toInt();
+        QAxObject* cCell = sheet->querySubObject("Cells(int,int)",r,1);
+        int data = cCell->dynamicCall("Value()").toInt();
+        QString s = QString::number(data);
+        qDebug()<<s<<endl;
+
     }
+
+    putText(image,"a",Point2f(100,100),FONT_HERSHEY_PLAIN,2,  Scalar(0,0,255), 2 , 8 ,false);
+  namedWindow("Image Window image",CV_WINDOW_AUTOSIZE);
+   imshow("Image Window image",image);
     return a.exec();
 
 }
--- membacadataexcel/membacadataexcel.pro
+++ membacadataexcel/membacadataexcel.pro
@@ -17,6 +17,17 @@ TEMPLATE = app
 # deprecated API in order to know how to port your code away from it.
 DEFINES += QT_DEPRECATED_WARNINGS
 
+INCLUDEPATH +=C:\OpenCV\opencv\build\install\include
+
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_core343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_highgui343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_imgcodecs343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_imgproc343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_calib3d343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_videoio343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_photo343.dll
+LIBS +=C:\OpenCV\opencv\build\bin\libopencv_video343.dll
+
 # You can also make your code fail to compile if you use deprecated APIs.
 # In order to do so, uncomment the following line.
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
