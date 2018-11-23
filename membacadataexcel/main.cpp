2f7e3022268bef53dc35c2a68daaf3e5ce280891
ariku <ariksugianto01@gmail.com>
2018-11-23 10:27:27 +0700

    menyatukan tiga waktu

--- membacadataexcel/main.cpp
+++ membacadataexcel/main.cpp
@@ -12,27 +12,43 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    Mat image =imread("D:\\Capture gambar\\gambar.png");
+   // Mat image =imread("D:\\Capture gambar\\gambar.png");
     QAxObject* excel     = new QAxObject("Excel.Application");
     QAxObject* workbooks = excel->querySubObject("Workbooks");
-    QString d_filename = "C:\\Users\\Ariku\\Documents\\Data.xlsx";
+  //  QString d_filename = "D:\\contoh.xlsx"; //hapus coment kalau mau run
     QAxObject* workbook  = workbooks->querySubObject("Open(const QString&)",d_filename);
     QAxObject* sheets    = workbook->querySubObject("Worksheets");
     QAxObject* sheet     = sheets->querySubObject("Item(int)", 1);
-
+    QAxObject * range = sheet->querySubObject("UsedRange");
+    QAxObject * rows = range->querySubObject( "Rows" );
+    int rowCount = rows->dynamicCall( "Count()" ).toInt();
+   QAxObject* columns = range->querySubObject( "Columns" );
+    int columnCount = columns->property("Count").toInt();
+   // qDebug()<<rowCount*columnCount<<endl;
+    int data[100];
     // read the first cells in row 1...18177
-    for (int r = 1; (r <= 1); ++r)
+    for (int r = 1; (r <= rowCount*columnCount); ++r)
     {
-        QAxObject* cCell = sheet->querySubObject("Cells(int,int)",r,1);
-        int data = cCell->dynamicCall("Value()").toInt();
-        QString s = QString::number(data);
-        qDebug()<<s<<endl;
-
+        if(r<=rowCount){
+            QAxObject* cCell = sheet->querySubObject("Cells(int,int)",r,1);
+             data[r]= cCell->dynamicCall("Value()").toInt();
+        }else if((r>rowCount)&&(r<=rowCount*2)){
+            QAxObject* cCell = sheet->querySubObject("Cells(int,int)",r-(rowCount),2);
+             data[r]= cCell->dynamicCall("Value()").toInt();
+        }else{
+            QAxObject* cCell = sheet->querySubObject("Cells(int,int)",r-(rowCount*2),3);
+             data[r]= cCell->dynamicCall("Value()").toInt();
+             if(data[r]==0){
+                 break;
+             }
+        }
+       // QString s = QString::number(data);
+        qDebug()<<data[r]<<endl;
     }
 
-    putText(image,"a",Point2f(100,100),FONT_HERSHEY_PLAIN,2,  Scalar(0,0,255), 2 , 8 ,false);
-  namedWindow("Image Window image",CV_WINDOW_AUTOSIZE);
-   imshow("Image Window image",image);
+    //putText(image,"a",Point2f(100,100),FONT_HERSHEY_PLAIN,2,  Scalar(0,0,255), 2 , 8 ,false);
+  //namedWindow("Image Window image",CV_WINDOW_AUTOSIZE);
+   //imshow("Image Window image",image);
     return a.exec();
 
 }
